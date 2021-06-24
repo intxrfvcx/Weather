8b14f5b7ba9585c3d8f06b85124f529f331b16e6
interface <interfacehunter@yandex.ru>
2021-06-24 14:59:28 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by interface on 24.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
